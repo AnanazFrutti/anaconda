4779f81d9b7934530c2f25fe656a60704d0668b5
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2018-11-06 23:43:40 +0100

    Bilder auf Bilder mit durchsichtigen, rotierenden Keywords

--- main.js
+++ main.js
@@ -6,5 +6,28 @@ var calculateThumbnailHeight = (function () {
        console.log(patternist02Height);
    });
 
+
+   // parallax
+
+var nononoyesno = document.querySelector(".textThumbnail");
+var bigCircle = document.querySelector(".circleBig");
+
+function setTranslate(xPos, yPos, el) {
+   el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+}
+
+window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+var xScrollPosition;
+var yScrollPosition;
+
+function scrollLoop() {
+   xScrollPosition = window.scrollX;
+   yScrollPosition = window.scrollY;
+   setTranslate(0, yScrollPosition * -0.4, nononoyesno);
+   setTranslate(0, yScrollPosition * -0.8, bigCircle);
+   requestAnimationFrame(scrollLoop);
+}
+
 $(document).ready(calculateThumbnailHeight);
 $(window).resize(calculateThumbnailHeight);
