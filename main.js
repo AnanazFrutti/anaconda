71a5ed2a7c17847a21b3dc4b218f5b41c75f11b9
Anna-Luise Lorenz <annaluise.lorenz@gmail.com>
2018-11-06 18:44:47 +0100

    Projecttitle: add flexible height on browsersize and test with two images

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+var calculateThumbnailHeight = (function () {
+       var patternist02Height = $('#patternist02').height();
+       $('.clipProjecttitle').css('height', patternist02Height);
+       var patternist03Height = $('#patternist03').height();
+       $('.clipProjecttitle2').css('height', patternist03Height);
+       console.log(patternist02Height);
+   });
 
+$(document).ready(calculateThumbnailHeight);
+$(window).resize(calculateThumbnailHeight);
